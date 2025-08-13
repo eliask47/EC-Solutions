9c2165dfc2b654d8de7dc8ed0de60bcacd652e0a
eliask47 <163206816+eliask47@users.noreply.github.com>
2025-08-13 10:42:12 -0400

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90 transition-all duration-300",
+        default: "bg-primary text-primary-foreground hover:bg-transparent hover:border hover:border-primary transition-all duration-300"
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
