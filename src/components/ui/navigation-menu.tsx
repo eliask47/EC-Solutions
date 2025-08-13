3f627603f5fea8ba7343dbfb1c7b098ca557a49b
eliask47 <163206816+eliask47@users.noreply.github.com>
2025-08-13 10:48:13 -0400

    Update navigation-menu.tsx

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -40,8 +40,9 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName
 
 const NavigationMenuItem = NavigationMenuPrimitive.Item
 
+// Option #3 hover style applied here
 const navigationMenuTriggerStyle = cva(
-  "group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+  "group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors border border-transparent hover:bg-transparent hover:border-primary hover:text-primary focus:bg-transparent focus:border-primary focus:text-primary focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:border-primary data-[state=open]:border-primary"
 )
 
 const NavigationMenuTrigger = React.forwardRef<
